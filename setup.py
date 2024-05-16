8e7bebaf0f8bba92a38070ddc8f49bc98b9a3888
Amar <amarnath.arjsd@gmail.com>
2024-05-16 20:51:52 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI Project',
+    version = '0.0.0',
+    author='amar',
+    author_email='amarnath.arjsd@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
